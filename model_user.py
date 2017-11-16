5b3c87c95da1578fc92eb83f9c989389e5c7ab05
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-11-16 17:38:11 +0100

    Fill set_first_name(), add new_first_name parameter

--- model_user.py
+++ model_user.py
@@ -15,10 +15,12 @@ class ModelUser():
         self.login = "{}-{}-{}".format(self.first_name, self.last_name, self.uid)
 
     def set_password(self, new_password):
-        self.set_password = 
+        self.set_password = new_password
 
-    def set_first_name(self):
-        pass
+    def set_first_name(self, new_first_name):
+        self.first_name = new_first_name
+        self.set_email()
+        self.set_login()
 
-    def set_last_name(self):
+    def set_last_name(self, new_last_name):
         pass
