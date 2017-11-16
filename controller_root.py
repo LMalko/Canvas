bc791d0fffef269d97411ef5eeb1c7201d5316bf
Michal <michal.hanebach@gmail.com>
2017-11-16 17:48:23 +0100

    move container initialization from model to controller

--- controller_root.py
+++ controller_root.py
@@ -1,10 +1,20 @@
+from controller_attendance_container import *
+from controller_member_container import *
+from controller_task_container import *
+
+from model_attendance_container import *
+from model_member_container import *
+from model_task_container import *
+
+from model_root import *
+
 class ControllerRoot:
 
-    def __init__(self, associated_model):
-        pass
+    def __init__(self):
+        self.initialize_model()
 
-    def initialize_model():
-        pass
+    def initialize_model(self):
+        self.associated_model = ModelRoot()
 
     def login(user_login, input_password):
         '''
@@ -16,4 +26,10 @@ class ControllerRoot:
         '''
         Launches a user controller based on class of user_obj.
         '''
+        pass
+
+    def initialize_containers(self):
+        pass
+
+    def initial_admin_creation(self):
         pass
