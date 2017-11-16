fc8b888fecbb7898befa5a59b456ff1ab7bc94bb
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-11-16 20:45:38 +0100

    Fill take_user_input()

--- view_user.py
+++ view_user.py
@@ -1,7 +1,7 @@
 class ViewUser():
 
-    def take_user_input(self):
-        pass
+    def take_user_input(self, message):
+        return input(message)
 
     def display_collection(self):
         pass
