bdd8e70be081eb1f0ac0585884614e9045a5cc30
jarqprog <jarqprog@gmail.com>
2017-11-16 17:28:55 +0100

    add class ModelStudent

---
+++ model_student.py
@@ -0,0 +1,19 @@
+from model_user import ModelUser
+
+class ModelStudent(ModelUser):
+    role = 'Student'
+
+    def __init__(
+                    self,
+                    uid,
+                    first_name,
+                    last_name,
+                    password,
+                    my_group):
+
+        super().__init__(
+                        uid,
+                        first_name,
+                        last_name,
+                        password)
+        self.my_group = my_group
