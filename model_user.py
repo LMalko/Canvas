b4371bcf2c1715fa8e01b03d45f812af3c2692f8
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-11-16 16:58:10 +0100

    Create class ModelUser frame

---
+++ model_user.py
@@ -0,0 +1,19 @@
+class ModelUser():
+
+    def __init__(self):
+        pass
+
+    def set_uid(self):
+        pass
+
+    def set_login(self):
+        pass
+
+    def set_password(self):
+        pass
+
+    def set_first_name(self):
+        pass
+
+    def set_last_name(self):
+        pass
