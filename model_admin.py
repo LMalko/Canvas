cf2bc1d7f4ffde9c7c31589ec8e81909d63e936e
LMalko <lmalko2601@gmail.com>
2017-11-16 17:39:05 +0100

    Added import and 'self' parameter to constructor

--- model_admin.py
+++ model_admin.py
@@ -1,9 +1,15 @@
-class ModelAdmin():
+from model_user import ModelUser
+
+
+class ModelAdmin(ModelUser):
 
     role = "admin"
 
-    def __init__(uid, first_name, last_name, password):
+    def __init__(self, uid, first_name, last_name, password):
         self.uid = uid
         self.first_name = first_name
         self.last_name = last_name
         self.password = password
+
+
+
