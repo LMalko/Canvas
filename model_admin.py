a7686dac6c4b35fe6b091b593cfffd5e172735ea
LMalko <lmalko2601@gmail.com>
2017-11-16 17:05:53 +0100

    Defined function

--- model_admin.py
+++ model_admin.py
@@ -1,1 +1,9 @@
-pass
+class ModelAdmin():
+
+    role = "admin"
+
+    def __init__(uid, first_name, last_name, password):
+        self.uid = uid
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
