ede2b8a5a09efca1cb65ccff6f780c83fb74d0b1
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-11-16 22:14:50 +0100

    Correct variables names

--- controller_user.py
+++ controller_user.py
@@ -11,28 +11,28 @@ class ControllerUser():
 
         while True:
             # print list of students - metoda z membera
-            user_id = view.get_user_input("Choose user by id: ")
+            user = self.view.get_user_input("Choose user by id: ")
             # user = get user object from container, input
             if user is not None:
                 return user
             self.view.display_message("No such user. Try again.")
 
     def change_first_name(self):
-        new_first_name = self.validate_input("Pass new first name: ")
         user = self.__get_user()
+        new_first_name = self.validate_input("Pass new first name: ")
         user.set_first_name(new_name)
         self.view.display_message("Name has been changed!")
 
     def change_last_name(self):
-        new_last_name = self.validate_input("Pass new last name: ")
         user = self.__get_user()
+        new_last_name = self.validate_input("Pass new last name: ")
         user.set_last_name(new_last_name)
         self.view.display_message("Last name has been changed!")
 
 
     def change_password(self):
-        new_password = self.validate_input("Pass new password (not shorter than 6 chars): ")
         user = self.__get_user()
+        new_password = self.validate_input("Pass new password (not shorter than 6 chars): ")
         user.set_first_name(new_password)
         self.view.display_message("Password has been changed!")
 
