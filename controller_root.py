6ea32805138d51651cf06164f6a0b7907cb6126d
Michal <michal.hanebach@gmail.com>
2017-11-16 16:59:36 +0100

    initial ctrl_root impl

---
+++ controller_root.py
@@ -0,0 +1,19 @@
+class ControllerRoot:
+
+    def __init__(self, associated_model):
+        pass
+
+    def initialize_model():
+        pass
+
+    def login(user_login, input_password):
+        '''
+        Checks whether user password matches the input_password.
+        '''
+        pass
+
+    def engage_user_controller(user_obj):
+        '''
+        Launches a user controller based on class of user_obj.
+        '''
+        pass
