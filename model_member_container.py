e3e49d12b039eda2e0c0170e278f12ea72f86064
LMalko <lmalko2601@gmail.com>
2017-11-16 20:23:17 +0100

    Added missing self.

--- model_member_container.py
+++ model_member_container.py
@@ -6,11 +6,11 @@ class ModelMemberContainer():
     def __init__(self):
         self.members = ControllerMemberContainer().member_container
 
-    def add_member(User):
+    def add_member(self, User):
         ControllerMemberContainer().member_container.append(User)
 
-    def delete_member(User):
+    def delete_member(self, User):
         ControllerMemberContainer().delete_member(User.uid)
 
-    def get_member(uid):
+    def get_member(self, uid):
         ControllerMemberContainer().get_member(uid)
