d8f5e02949bc41e97de8f0ef8c03e9b1c8903a18
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-11-16 19:37:00 +0100

    Fill __get_user()

--- controller_user.py
+++ controller_user.py
@@ -7,12 +7,16 @@ class ControllerUser():
     def __init__(self, ModelMemberContainer):
         self.model_member_container = model_member_container
         self.view = ViewUser
-        
+
     def __get_user(self):
-        # print list of students
-        # get student id from input
-        # get user object from container
-        # return object
+
+        while True:
+            # print list of students - metoda z membera
+            user_id = view.get_user_input("Choose user by id: ")
+            # user = get user object from container, input
+            if user != None:
+                return user
+            self.view.display_message("No such user. Try again.")
 
     def change_first_name(self):
         # using getter get user obj
