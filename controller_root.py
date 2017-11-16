9a2b3d2e5ac6a65937f4fc4476761faabec87879
Michal <michal.hanebach@gmail.com>
2017-11-16 19:18:54 +0100

    implement container initialization

--- controller_root.py
+++ controller_root.py
@@ -1,20 +1,41 @@
-from controller_attendance_container import *
-from controller_member_container import *
-from controller_task_container import *
+'''
+import controller_attendance_container
+'''
+import controller_member_container
+'''
+import controller_task_container
 
-from model_attendance_container import *
-from model_member_container import *
-from model_task_container import *
+import model_attendance_container
+import model_member_container
+import model_task_container
 
+import dao_attendance
+import dao_member
+import dao_task
+'''
 from model_root import *
+
+import controller_admin
 
 class ControllerRoot:
 
     def __init__(self):
         self.initialize_model()
 
+
     def initialize_model(self):
         self.associated_model = ModelRoot()
+        self.initialize_containers()
+        if not self.associated_model.container_member:
+            # the user db is empty, instantiate memb. contain. control. to add first admin
+            member_ctrl = controller_member_container.ControllerMemberContainer(\
+                self.associated_model.container_member)
+
+            # create first admin
+            first_admin = initial_admin_creation()
+
+            # add it to the container via member container controller
+            member_ctrl.add_member(first_admin)
 
     def login(user_login, input_password):
         '''
@@ -29,7 +50,25 @@ class ControllerRoot:
         pass
 
     def initialize_containers(self):
-        pass
+        # uncomment below when DAOs are implemented
+        '''
+        # load members
+        member_dao = dao_member.DAOMember("members.csv")
+        members = dao_member.import_data()
+        self.associated_model.container_member = members
+        # load attendance
+        attendance_dao = dao_attendance.DAOAttendance("attendance.csv")
+        attendance = dao_attendance.import_data()
+        self.associated_model.container_attendance = attendance
+        # load tasks
+        task_dao = dao_task.DAOTask("tasks.csv")
+        tasks = dao_task.import_data()
+        self.associated_model.container_task = tasks
+        '''
+        self.associated_model.container_member = [] # we assume emptiness for now
+        self.associated_model.container_task = []
+        self.associated_model.container_attendance = []
 
     def initial_admin_creation(self):
-        pass
+        ctrl_admin = controller_admin.ControllerAdmin(None, None)
+        return ctrl_admin.create_first_admin()
