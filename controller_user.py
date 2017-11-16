739557a746960c48a197bd73907e14aaca1b0c7f
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-11-16 18:59:55 +0100

    Create __get_user(), add comment descriptions

--- controller_user.py
+++ controller_user.py
@@ -3,11 +3,19 @@ from model_user import *
 
 class ControllerUser():
 
-    def __init__(self, User):
-        self.user = User
-        
+    def __init__(self, ModelMemberContainer):
+        self.model_member_container = model_member_container
+    
+    def __get_user(self):
+        # print list of students
+        # get student id from input
+        # get user object from container
+        # return object
+
     def change_first_name(self):
-        pass
+        # using getter get user obj
+        # get new name from input
+        # call model_member_container on user, pas new name
 
     def change_lats_name(self):
         pass
