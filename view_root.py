5914f565f8e76e09db3eaedd92fc7c2496c92651
jarqprog <jarqprog@gmail.com>
2017-11-16 19:20:34 +0100

    add class & methods placeholder

---
+++ view_root.py
@@ -0,0 +1,35 @@
+
+class ViewRoot():
+
+    def display_login_screen(text):
+        pass
+
+    def take_user_input(text, valid_condition):
+        pass
+
+    def check_if_input_is_valid(valid_condition):
+        pass
+
+    def display_message(message):
+        pass
+
+    def display_text(text):
+        pass
+
+    def display_start_screen(text=''):
+        pass
+
+    def display_exit_screen(text=''):
+        pass
+
+
+  #
+  #
+  # + __init__(): None
+  # + display_login_screen(text): None
+  # + take_user_input(text, valid_condition): string
+  # + check_if_input_is_valid(valid_condition): bool
+  # + display_message(message): None
+  # + display_text(text): None
+  # + display_start_screen(text=''): None
+  # + display_exit_screen(text=''): None
