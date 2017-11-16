8919fc6d12d38621e9f85002442cc7fa297c0944
jarqprog <jarqprog@gmail.com>
2017-11-16 17:48:36 +0100

    add class ControllerStudent

---
+++ controller_student.py
@@ -0,0 +1,14 @@
+from model_student import ModelStudent
+
+
+class ControllerStudent():
+
+    def __init__(self, User):
+        self.associated_user = User
+
+    def submit_task(self):
+        pass
+        # self.associated_user.
+
+    def view_grades(self):
+        pass
