f896c2ca1dcd395652c55af468a0d04da526bb39
LMalko <lmalko2601@gmail.com>
2017-11-16 19:45:00 +0100

    Defined class DAOMember

---
+++ dao_member.py
@@ -0,0 +1,25 @@
+from model_member_container import ModelMemberContainer
+
+
+class DAOMember():
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def import_data(self):
+        return __extract_imported_data()
+
+    def __extract_imported_data(self):
+        self.imported_data = []
+        with open(self.filename, "r", encoding="utf-8") as myfile:
+            for item in myfile.split("\n"):
+                self.imported_data.append(line)
+        return self.imported_data
+
+    def export_data(self):
+        with open(self.filename, "w", encoding="utf-8") as myfile:
+            for i in self.data_for_export:
+                myfile.write(i)
+
+    def __pass_data_for_export(self):
+        return self.data_for_export = ModelMemberContainer.members
