74715422c38b1b34b264d31d476db15b47ad98b0
LMalko <lmalko2601@gmail.com>
2017-11-16 17:26:07 +0100

    Defined class

--- controller_admin.py
+++ controller_admin.py
@@ -1,1 +1,19 @@
-pass
+class ControllerAdmin():
+
+    def __init__(User):
+        associated_user = User
+
+    def add_mentor():
+        pass
+
+    def remove_mentor():
+        pass
+
+    def edit_mentor():
+        pass
+
+    def view_mentor_list():
+        pass
+
+    def view_student_list():
+        pass
