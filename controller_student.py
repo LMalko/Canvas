2a232d69674e52648a928e559ccf2428caaeaa40
jarqprog <jarqprog@gmail.com>
2017-11-16 18:08:53 +0100

    add submit task feature

--- controller_student.py
+++ controller_student.py
@@ -1,14 +1,20 @@
 from model_student import ModelStudent
+from controller_task_container import ControllerTaskContainer
+from view_student import ViewStudent
 
 
 class ControllerStudent():
 
     def __init__(self, User):
         self.associated_user = User
+        self.controller_task_container = ControllerTaskContainer()
+        self.view = ViewStudent()
 
     def submit_task(self):
-        pass
-        # self.associated_user.
+        # widoki
+        self.view.display()  # temp!!!
+        self.self.controller_task_container.change_delivery_status()  # którego zadania??
+        self.view.display()  # temp!!!
 
     def view_grades(self):
         pass
