cbfe402d4f17b96a422634bd1345079cb59528b1
LMalko <lmalko2601@gmail.com>
2017-11-16 20:35:39 +0100

    Defined class

---
+++ controller_member_container.py
@@ -0,0 +1,34 @@
+
+
+class ControllerMemberContainer():
+    
+    def __init__(self, ModelMemberContainer):
+        member_container = ModelMemberContainer
+
+
+    def change_member_first_name(self, uid):
+        member_container
+
+    def change_member_last_name(self, uid):
+        
+
+    def change_member_password(self, uid):
+        
+    
+    def change_member_group(self, uid, new_group):
+        
+
+    def get_member(self, uid):
+        for i in member_container:
+            if i.uid = uid str
+
+    def get_members_by_role(self, role): list
+
+    def get_members_by_group(self, group): list
+
+    def add_member(self, User): None
+
+    def delete_member(self, uid): None
+
+
+        
