61bb86f365a967e82fccb2a45a5ba029d6efb9b2
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-11-16 18:01:47 +0100

    Create class ControllerUser frame)

---
+++ controller_user.py
@@ -0,0 +1,19 @@
+class ControllerUser():
+
+    def change_first_name(self):
+        pass
+
+    def change_lats_name(self):
+        pass
+
+    def change_password(self):
+        pass
+
+    def display_list(self):
+        pass
+
+    def display_message(self):
+        pass
+
+    def get_user_input(self):
+        pass
