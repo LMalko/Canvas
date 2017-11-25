ea852196d4f9f75985e4d83176974053c35cdac9
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-11-25 22:36:09 +0100

    Fill export_data() & __pack_data_for_export() in DaoTask class

--- dao_task.py
+++ dao_task.py
@@ -1,9 +1,8 @@
-from model_root import ModelRoot
 from controller_task_container import *
 from controller_task import *
 
 
-class DAOMember():
+class DAOTask():
 
     def __init__(self, filename):
         self.filename = filename
@@ -24,10 +23,17 @@ class DAOMember():
             tasks_collection.append(self.ctrl_task.create_task_from_imported_data(*data))
         return tasks_collection
 
-    def export_data(self):
+    def export_data(self, tasks_collection):
+        data_to_export = self.__pack_data_for_export(tasks_collection)
         with open(self.filename, "w", encoding="utf-8") as myfile:
-            for i in self.data_for_export:
-                myfile.write(i)
+            for line in data_to_export:
+                myfile.write(line)
 
-    def __pass_data_for_export(self):
-        return self.data_for_export = ControllerMemberContainer().member_container
+    def __pack_data_for_export(self, tasks_collection):
+        data_to_export = []
+        for task in tasks_collection:
+            data = "|".join(self.ctrl_task.get_task_data_to_export(task)) + '\n'
+            data_to_export.append(data)
+
+        return data_to_export
+
