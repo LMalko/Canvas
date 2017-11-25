77d4044adf3a1b1c0564131dcb67db768a5ae010
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-11-25 17:57:05 +0100

    Rewrite __extract_imported_data() in DAOMember class

--- dao_member.py
+++ dao_member.py
@@ -6,22 +6,22 @@ from controller_student import *
 from controller_member_container import *
 '''
 Model danych:
-role|uid|first_name|last|name|password|my_group
+role|uid|first_name|last_name|password|my_group
 '''
 
 class DAOMember():
-'''
-    role_class_pairs dict contains:
-            keys - class attribute 'role' ((str)
-            values - class ModelUser object
-'''
+    '''
+        controller_model_pairs dict contains:
+                keys - Controller (class)
+                values - ModelUser (class)
+    '''
     def __init__(self, filename):
         self.filename = filename
-        self.role_class_pairs = {}
-        self.role_class_pairs.update(ControllerAdmin.get_role_class_pair())
-        self.role_class_pairs.update(ControllerMentor.get_role_class_pair())
-        self.role_class_pairs.update(ControllerStudent.get_role_class_pair())
-        self.role_class_pairs.update(ControllerOffice.get_role_class_pair())
+        self.controller_model_pairs = {}
+        self.controller_model_pairs.update(ControllerAdmin.get_role_class_pair())
+        self.controller_model_pairs.update(ControllerMentor.get_role_class_pair())
+        self.controller_model_pairs.update(ControllerStudent.get_role_class_pair())
+        self.controller_model_pairs.update(ControllerOffice.get_role_class_pair())
 
     def import_data(self):
         with open(self.filename, "r", encoding="utf-8") as myfile:
@@ -32,7 +32,10 @@ class DAOMember():
         imported_data = []
         for line in file_content:
             user_role, *args = line.split('|')
-            imported_data.append(self.role_class_pairs[user_role](*args))
+            for ctrl in self.controller_model_pairs:
+                if ctrl.get_user_role(self.controller_model_pairs[ctrl]) == user_role:
+                    import_data.append(ctrl.create_user_from_imported_data(*args))
+                    break
         
         return imported_data
 
