6fcd8d3b05d9e43ed51e42da1017c4b48292a343
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-11-25 22:05:33 +0100

    Create & fill get_user_data_to_export() in ControllerTask class

--- controller_task.py
+++ controller_task.py
@@ -6,3 +6,5 @@ class ControllerTask():
     def create_task_from_imported_data(self, *args):
         return ModelTask(*args)
 
+    def get_user_data_to_export(self, task):
+        return task.get_user_data_to_export()
