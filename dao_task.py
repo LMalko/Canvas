6361c7a2a529d006fcc70d589b9cc7b14eeffb75
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-11-25 22:19:17 +0100

    Fill import_data() & __extract_imported_data() in DaoTask class

--- dao_task.py
+++ dao_task.py
@@ -1,20 +1,28 @@
 from model_root import ModelRoot
+from controller_task_container import *
+from controller_task import *
 
 
 class DAOMember():
 
     def __init__(self, filename):
         self.filename = filename
+        self.ctrl_task = ControllerTask()
 
     def import_data(self):
-        return __extract_imported_data()
-
-    def __extract_imported_data(self):
-        self.imported_data = []
+        imported_data = []
         with open(self.filename, "r", encoding="utf-8") as myfile:
-            for item in myfile.split("\n"):
-                self.imported_data.append(line)
-        return self.imported_data
+            for line in myfile.split("\n"):
+                imported_data.append(line)
+
+        return __extract_imported_data(imported_data)
+
+    def __extract_imported_data(self, imported_data):
+        tasks_collection = []
+        for line in imported_data:
+            data = line.split('|')
+            tasks_collection.append(self.ctrl_task.create_task_from_imported_data(*data))
+        return tasks_collection
 
     def export_data(self):
         with open(self.filename, "w", encoding="utf-8") as myfile:
