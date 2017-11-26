5a295d1ee8586cbefff7160c0e30a3c4f478d8fb
jarqprog <jarqprog@gmail.com>
2017-11-26 11:29:39 +0100

    add ControllerAttendance, refactor ModelAttendance (add set method instead add&change methods)

--- model_attendance.py
+++ model_attendance.py
@@ -3,11 +3,8 @@ class ModelAttendance():
     def __init__(self):
         self.user_presence = {}
 
-    def add_presence_status(self, actuall_date, status):
+    def set_presence_status(self, actuall_date, status):
         self.user_presence.update({actuall_date: status})
-
-    def change_presence_status(self, date, status):
-        self.user_presence[date] = status
 
     def count_attendance_percentage(self):
         return (sum(self.user_presence.values()) / len(self.user_presence)) * 100
