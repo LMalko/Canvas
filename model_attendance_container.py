578437846f8305636eecf115bf0db2fefeb2f5fb
jarqprog <jarqprog@gmail.com>
2017-11-26 12:30:33 +0100

    refactor

--- model_attendance_container.py
+++ model_attendance_container.py
@@ -3,8 +3,8 @@ class ModelAttendanceContainer():
     def __init__(self):
         self.attendance = {}
 
-    def add_user(self, user_id, ModelAttendance):
-        self.attendance.update({user_id: ModelAttendance})
+    def add_user(self, user_id, user_presence):
+        self.attendance.update({user_id: user_presence})
 
     def del_user(self, user_id):
         if user_id in self.attendance:
