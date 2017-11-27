1920468e6617c6b76974a1891a653b57eb34a0fc
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-11-27 13:55:49 +0100

    Import ModelTask & ModelTaskContainer; Fill constructor

--- controller_task_container.py
+++ controller_task_container.py
@@ -1,6 +1,8 @@
-
+from model_task import*
+from model_attendance_container import*
 
 class ControllerTaskContainer():
 
-    def __init__(self, ContainerTask):
-        pass
+    def __init__(self, container_task):
+        self.container_task = container_task
+
