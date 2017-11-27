04de103d3824488f6ce72d601c09927b4f109aa6
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-11-27 16:39:46 +0100

    Fill create_task()

--- controller_task_container.py
+++ controller_task_container.py
@@ -1,15 +1,16 @@
 from model_task import*
 from model_attendance_container import*
 from view_controller_task_container import*
+from controller_student import *
 
 class ControllerTaskContainer():
 
-    def __init__(self, container_task):
+    def __init__(self, container_member, container_task):
         self.container_task = container_task
         self.view_task_container = ViewControllerTaskContainer()
+        self.ctrl_student = ControllerStudent()
 
-    def add_task_to_container(self):
-        task = self.create_task()
+    def add_task_to_container(self, task):
         self.container_task.add_task(task)
 
     def del_task_from_container(self):
@@ -34,15 +35,22 @@ class ControllerTaskContainer():
             all_id.append(int(task.get_task_id()))
         return "{:0>4}".format(max(all_id)+1)
 
-
-
     def __get_task(self):
         pass
 
     def get_taks_id(self):
         pass
 
-    def create_task(self, name, user_id, task_id):
+    def create_task(self):
+        task_id = self.get_max_task_id
+        task_name = self.get_valid_input()
+        target_group = self.get_target_group_for_task_deployment()  #zt listę obiektów studentów DOKOŃCZYĆ!!!
+        for student in target_group:
+            student_id = self.ctrl_student.get_member_id(student)
+            self.add_task_to_container(ModelTask(task_name, task_id, student_id))
+
+    def get_target_group_for_task_deployment(self):
+        #### z members container gest students group, wybieranie grupy już tam, zt listę obiektów studentów danej grupy
         pass
     
     def get_valid_input(self, user_input):
