c309e57cd8b0705e2e5a4b855f61444e87b000dd
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-11-27 14:09:38 +0100

    Create ModelTaskContainer class frame

---
+++ model_task_container.py
@@ -0,0 +1,17 @@
+class ModelTaskContainer():
+
+    def __init__(self):
+        self.task_container = []
+
+    def add_task(self, ModelTask):
+        pass
+
+    def del_task(self, ModelTask):
+        pass
+
+    def get_task(self, task_id):
+        pass
+
+    def get_max_id(self):
+        pass
+
