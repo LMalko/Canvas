73a41750e6dfd4033ee22e26a207fd81d99205f9
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-11-28 23:49:34 +0100

    Rename methods returning info to display

--- model_task.py
+++ model_task.py
@@ -10,22 +10,19 @@ class ModelTask():
         self.is_graded = is_graded
 
     def change_delivery_status(self):
-
+        
         self.is_done = 1 if not self.is_done else 0
         if self.is_done == 0:
             self.grade = None
             self.is_graded = 0
 
     def mark_as_graded(self):
-
         self.is_graded = 1
 
     def rename_task(self, new_name):
-
         self.name = new_name
 
     def set_grade(self, grade):
-
         self.grade = grade
 
     def get_task_id(self):
@@ -37,9 +34,16 @@ class ModelTask():
     def get_task_name(self):
         return self.name
 
-    def task_display(self):
+    def get_mentor_task_display(self):
         done = 'DELIVERED' if self.is_done else 'NOT DELIVERED'
         return "Task: '{}', ID: {}, student: {}, grade {}, status: {}".format(self.name, self.task_ID, self.user_ID, self.grade, done)
+    
+    def get_student_task_display(self):
+        done = 'DELIVERED' if self.is_done else 'NOT DELIVERED'
+        return "Task: '{}', ID: {}, grade {}, status: {}".format(self.name, self.task_ID, self.grade, done)
+    
+    def get_short_task_display(self):
+        return "{}: {}".format(self.task_ID, self.name)
     
     def get_data_to_export(self):
         return [self.name,
@@ -49,9 +53,3 @@ class ModelTask():
                 str(self.is_done),
                 str(self.is_graded)]
 
-    def get_student_task_display(self):
-        done = 'DELIVERED' if self.is_done else 'NOT DELIVERED'
-        return "Task: '{}', ID: {}, grade {}, status: {}".format(self.name, self.task_ID, self.grade, done)
-    
-    def get_task_display(self):
-        return "{}: {}".format(self.task_ID, self.name)
