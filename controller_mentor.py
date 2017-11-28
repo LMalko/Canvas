c236820105fd3fc62b8617bda9c981f70df16a9b
LMalko86 <lmalko2601@gmail.com>
2017-11-28 13:49:49 +0100

    Defined start method

--- controller_mentor.py
+++ controller_mentor.py
@@ -23,7 +23,49 @@ class ControllerMentor(ControllerUser):
         self.controller_member_container = ControllerMemberContainer(member_container)
 
     def start(self):
-        pass
+        self.view.clear_screen()
+        choices = ["1: View student list",
+                   "2: Edit student",
+                   "3: Add student",
+                   "4: View task list"
+                   "5: Grade task",
+                   "6: Add task",
+                   "7: Edit task",
+                   "8: Check attendance",
+                   "9: Get random groups of two",
+                   "10: Get random groups of four",
+                   "11: Log out"]
+        correct_choices = [str(x+1) for x in range(1, len(choices))]
+        message = "\nPlease, type Your choice: "
+        to_continue = True
+        while to_continue:
+            user_input = ''
+            while user_input not in correct_choices:
+                self.view.clear_screen()
+                self.view.display_collection(choices)
+                user_input = self.view.get_user_input(message)
+                if user_input == "1":
+                    self.get_members_display(ControllerMemberContainer.get_members_by_role('student'))
+                elif user_input == "2":
+
+                elif user_input == "3":
+
+                elif user_input == "4":
+
+                elif user_input == "5":
+
+                elif user_input == "6":
+
+                elif user_input == "7":
+
+                elif user_input == "8":
+
+                elif user_input == "9":
+                    self.view.display_collection(self.get_random_student_group())  
+                elif user_input == "10":
+                    self.view.display_collection(self.get_random_student_group(4)) 
+                elif user_input == "11":
+                    to_continue = False
 
     def create_mentor(self, first_name, last_name, password, my_group):
         '''Return ModelMentor object.'''
@@ -36,8 +78,9 @@ class ControllerMentor(ControllerUser):
         # self.self.controller_task_container.change_delivery_status()  # którego zadania??
         # self.view.display()  # temp!!!
 
-    def view_student_list(self):
-        pass
+    def get_members_display(self, members):
+        for person in members:
+            self.view.display_message(self.controller_user.get_member_display(person))
 
     def grade_task(self):
         pass
