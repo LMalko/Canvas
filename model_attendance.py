1c63055b3173bdad346278f53b9ca7e89ddd0005
jarqprog <jarqprog@gmail.com>
2017-11-28 15:18:10 +0100

    refactor, add rename user/student, add __str__, __repr__ methods

--- model_attendance.py
+++ model_attendance.py
@@ -1,17 +1,34 @@
 class ModelAttendance():
 
-    def __init__(self):
-        self.user_presence = {}
+    def __init__(self, student_uid):
+        self.student_uid = student_uid
+        self.student_presence = {}
+
+    def set_student_uid(self, new_student_uid):
+        self.student_uid = new_student_uid
 
     def set_presence_status(self, actuall_date, status):
-        self.user_presence.update({actuall_date: status})
+        self.student_presence.update({actuall_date: status})
 
     def count_attendance_percentage(self):
         percentage_multiplier = 100
-        return (sum(self.user_presence.values()) / len(self.user_presence)) * percentage_multiplier
+        return (sum(self.student_presence.values()) / len(self.student_presence)) * percentage_multiplier
 
-    def get_user_presence(self):
-        return self.user_presence
+    def get_student_uid(self):
+        return self.student_uid
 
-    def get_data_to_export(self):
-        pass  # dla Ani :)
+    def get_student_presence(self):
+        return self.student_presence
+
+    def __str__(self):
+        return 'attendance for student UID: {}'.format(self.student_uid)
+
+    def __repr__(self):
+        return 'ModelAttendance({})'.format(self.student_uid)
+
+    # def get_data_to_export(self):
+    #     data_to_export = [self.student_uid]
+    #     for date, presence in self.student_presence.items():
+    #         data_to_export.append(date)
+    #         data_to_export.append(str(presence))
+    #     return data_to_export
