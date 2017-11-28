fc02158be50e5ce62541dd25ea7a6c7afc1f57e4
LMalko86 <lmalko2601@gmail.com>
2017-11-28 17:58:55 +0100

    Added method display collection

--- view_member_container.py
+++ view_member_container.py
@@ -2,9 +2,13 @@ class ViewMemberContainer():
 
     def __init__(self):
         pass
-    
+
     def display_message(self, message):
         print("\n\n" + message + "\n\n")
 
     def take_user_input(self, message):
         return input(message)
+
+    def display_collection(self, collection):
+        for item in collection:
+            print(item)
