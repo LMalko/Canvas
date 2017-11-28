06681794a8d5761a150458fca28b80a055640602
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-11-28 12:16:03 +0100

    Add self to all methods

--- view_member_container.py
+++ view_member_container.py
@@ -3,8 +3,8 @@ class ViewMemberContainer():
     def __init__(self):
         pass
     
-    def display_message(message):
+    def display_message(self, message):
         print("\n\n" + message + "\n\n")
 
-    def take_user_input(message):
+    def take_user_input(self, message):
         return input(message)
