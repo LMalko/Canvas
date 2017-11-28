3bbc0d5158e0e2cafe9a8f8c8cda7d47122e2577
Michal <michal.hanebach@gmail.com>
2017-11-28 17:09:16 +0100

    plugged in the Attendence ecosystem: DAOAttendance, ModelAttendanceContainer, ControllerAttendanceContainer

--- controller_root.py
+++ controller_root.py
@@ -1,29 +1,22 @@
-'''
-import controller_attendance_container
-'''
+from controller_attendance_container import ControllerAttendanceContainer
 from controller_member_container import ControllerMemberContainer
 from controller_task_container import ControllerTaskContainer
-'''
-import model_attendance_container
-'''
+
+from model_attendance_container import ModelAttendanceContainer
 from model_member_container import ModelMemberContainer
 from model_task_container import ModelTaskContainer
-'''
+
 import dao_attendance
-'''
 import dao_member
 import dao_task
 
 from controller_admin import ControllerAdmin
-
 from controller_mentor import ControllerMentor
 from controller_office import ControllerOffice
 from controller_student import ControllerStudent
 
 import view_root
 from model_root import ModelRoot
-
-import controller_admin
 
 class ControllerRoot:
 
@@ -102,19 +95,18 @@ class ControllerRoot:
 
         # uncomment below when appropriate DAOs are implemented
         self.associated_model.container_attendance = ModelAttendanceContainer() # empty
-        '''
+
         # load attendance
-        attendance_dao = dao_attendance.DAOAttendance("attendance.csv")
+        attendance_dao = dao_attendance.DAOAttendance()
         attendance_object_collection = dao_attendance.import_data()
         # initialize attendance container object
         attendance_container = ModelAttendanceContainer()
         for attendance in attendance_object_collection:
             attendance_container.add_student_attendance(attendance)
         self.associated_model.container_attendance = attendance_container
-        '''
 
     def initial_admin_creation(self):
-        ctrl_admin = controller_admin.ControllerAdmin(None, None)
+        ctrl_admin = ControllerAdmin(None, None)
         return ctrl_admin.create_first_admin()
 
     def start(self):
