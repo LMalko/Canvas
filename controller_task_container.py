efa640d0de883c28354cdffaa040cccfb52328da
Michal <michal.hanebach@gmail.com>
2017-11-29 11:19:43 +0100

    ControllerTaskContainer: deleted reception of member container (it had been for testing purposes)

--- controller_task_container.py
+++ controller_task_container.py
@@ -6,7 +6,7 @@ from controller_user import*
 
 class ControllerTaskContainer():
 
-    def __init__(self, container_member, container_task):
+    def __init__(self, container_task):
         self.container_task = container_task
         self.view_task_container = ViewControllerTaskContainer()
         self.ctrl_user = ControllerUser()
@@ -66,11 +66,11 @@ class ControllerTaskContainer():
         task_id = self.get_max_task_id()
         task_name = self.get_valid_input("Pass tasks name: ")
         target_group = target_group
-        
+
         for student in target_group:
             student_id = self.ctrl_user.get_member_id(student) # Pobrać liste id zamiast listy obiektów, bo to jedyne odwołanie do CtrlUser
             self.add_task_to_container(ModelTask(task_name, task_id, student_id))
-    
+
     def get_valid_input(self, message):
         invalid_input = True
         while invalid_input:
@@ -90,10 +90,10 @@ class ControllerTaskContainer():
     def get_task_by_genre(self):
         all_tasks = self.container_task.get_all_tasks()
         tasks_by_id = []
-        
+
         for task in all_tasks:
             tasks_by_id.append(task.get_short_task_display())
-        
+
         invalid_choice = True
         while invalid_choice:
             self.view_task_container.display_collection(sorted(list(set(tasks_by_id))))  # zamienic na fora, usunac collection z view
@@ -104,7 +104,7 @@ class ControllerTaskContainer():
                     break
 
         return user_choice
-        
+
     def take_and_validate_task_choice(self, all_tasks):
 
         invalid_choice = True
@@ -143,9 +143,9 @@ class ControllerTaskContainer():
         for task in task_collection:
             if task.get_user_id() == user_id:
                 user_tasks.append(task)
-        
+
         return user_tasks
-        
+
 
 # from dao_task import *
 # from model_task_container import*
