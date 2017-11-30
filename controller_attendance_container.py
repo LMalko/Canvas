2b5596972d10ad38c09a2c1e62b49b9ce9543643
Michal <michal.hanebach@gmail.com>
2017-11-30 13:31:43 +0100

    ControllerAttendanceContainer: refactor confusing symbols

--- controller_attendance_container.py
+++ controller_attendance_container.py
@@ -3,8 +3,8 @@ from model_attendance import ModelAttendance
 
 class ControllerAttendanceContainer():
 
-    def __init__(self, ModelAttendanceContainer):
-        self.attendance_container = ModelAttendanceContainer
+    def __init__(self, attendance_container_object):
+        self.attendance_container = attendance_container_object
 
     def create_student_attendance(self, student_uid):
         return ModelAttendance(student_uid)
@@ -12,15 +12,15 @@ class ControllerAttendanceContainer():
     def set_new_attendance(self, new_collection):
         self.attendance_container.set_new_attendance(new_collection)
 
-    def add_student_attendance(self, ModelAttendance):
-        self.attendance_container.add_student_attendance(ModelAttendance)
+    def add_student_attendance(self, attendance_object):
+        self.attendance_container.add_student_attendance(attendance_object)
 
     def create_student_attendance_and_add_to_container(self, student_uid):
         self.attendance_container.add_student_attendance(
                                         self.create_student_attendance(student_uid))
 
-    def del_student_attendance(self, ModelAttendance):
-        self.attendance_container.del_student_attendance(ModelAttendance)
+    def del_student_attendance(self, attendance_object):
+        self.attendance_container.del_student_attendance(attendance_object)
 
     def set_student_presence_status_for_given_date(self, student_uid, given_date, status):
         self.attendance_container.get_student_attendance(student_uid).set_presence_status(given_date, status)
