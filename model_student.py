075dab06e4818079b713c6323fd4a95198d5afc0
jarqprog <jarqprog@gmail.com>
2017-11-30 12:12:54 +0100

    add method get full data

--- model_student.py
+++ model_student.py
@@ -16,8 +16,8 @@ class ModelStudent(ModelUser):
     def get_my_group(self):
         return self.my_group
 
-    def __str__(self):
-        return '{} {}'.format(self.first_name, self.last_name)
+    def get_full_data(self):
+        return '{}\ngroup: {}'.format(super().get_full_data(), self.my_group)
 
     def get_data_for_export(self):
         return [self.role, self.uid, self.first_name, self.last_name, self.password, self.my_group]
